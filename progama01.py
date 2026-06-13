cffaa75dd4a237ac83fe31ae54796750dab557a1
WesleyDuarte00 <wesleyrsd16@gmail.com>
2026-06-12 21:25:36 -0300

    aprendendo variáiveis e impirmir tela

---
+++ progama01.py
@@ -0,0 +1,20 @@
+nome = "Wesley"
+ultimo_nome = "Duarte"
+idade = 19
+altura = 1.99
+maior_de_idade = True
+
+print("Olá", nome, ultimo_nome,  "você tem", idade, "anos, sua altura é", altura,"você é maior de idade?", maior_de_idade, "!")
+print(f"Olá {nome} {ultimo_nome} você tem {idade} anos, sua altura é {altura}, você é maior de idade?", maior_de_idade, "!")
+
+
+
+
+
+
+
+
+
+
+
+
