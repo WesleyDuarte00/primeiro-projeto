6d5beeddfe57cdb93e0f57df19f379b71c073d3e
WesleyDuarte00 <wesleyrsd16@gmail.com>
2026-06-15 21:24:14 -0300

    NOVO COMMIT

---
+++ programa 02.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+nome_amigo = input("Digite o nome do seu melhor amigo: ")
+
+print(f"olá {nome} seja bem vindo ao programa!")
+
+ano_nasc = int(input(f"Prazer em te conhecer {nome}!, \n digite seu ano de nascimento: "))
+
+print(f"o seu melhor amigo é o {nome_amigo}")
---
+++ programa 03.py
@@ -0,0 +1,21 @@
+# Calculadora python
+
+num1 = int(input("Digite o primeiro número: "))
+
+num2 = int(input("Digite p segundo número: "))
+
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é igual a = {soma}")
+subtracao = num1 - num2
+print(f"A subtracao de {num1} e {num2} é igual a = {subtracao}")
+multiplicacao = num1 * num2
+print(f"A multiplicacao de {num1} e {num2} é igual a = {multiplicacao}")
+divisao = num1 / num2
+print(f"A divisao de {num1} e {num2} é igual a = {divisao}")
+divisao_inteira = num1//num2
+print(f"A divisao_inteira de {num1} e {num2} é igual a = {divisao_inteira}")
+resto_divisao = num1 % num2
+print(f"A resto_divisao de {num1} e {num2} é igual a = {resto_divisao}")
+potenciacao = num1 ** num2
+print(f"A potenciacao de {num1} e {num2} é igual a = {potenciacao}")
+
